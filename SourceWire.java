1fff078cb59dba9e4e5a8f580eeb379e1be0233e
zencue <84475875+zencue@users.noreply.github.com>
2024-05-29 14:09:15 -0400

    Update SourceWire.java

--- SourceWire.java
+++ SourceWire.java
@@ -5,19 +5,31 @@ public class SourceWire extends Wire{
 	private Electron electron;
 	private int finish;
 	private int counter;
+	private final int[][] speeds = new int[4][2] ;
 	public SourceWire(int x, int y,Cell parent,int[] speed, String dir) {
 		
 		super(x,y,parent,Color.red, speed,  dir);
+		speeds[0][0] = 1;
+		speeds[0][1] = 0;
+		speeds[1][0] = -1;
+		speeds[1][1] = 0;
 		
+		speeds[2][1] = 1;
+		speeds[2][0] = 0;
+		speeds[3][1] = -1;
+		speeds[3][0] = 0;
 		counter = 0;
 		finish = 3;
 		wire = this;
-		createSignal();
+		createSignals();
 	}
-	public void createSignal() {
+	public void createSignals() {
 		if(counter == finish) {
-			electron = new Electron(this.getX(),this.getY(),getSpeed());
-			WireworldPlane.addElectron(electron);
+			for(int i =0;i<4;i++) {
+				electron = new Electron(this.getX(),this.getY(),speeds[i]);
+				WireworldPlane.addElectron(electron);
+			}
+			
 			counter = 0;
 		}
 		else {
