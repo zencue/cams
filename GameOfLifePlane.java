752762505e0da8c29b90ced9339c5f6f40501636
dawr3k <168124084+dawr3k@users.noreply.github.com>
2024-06-04 14:07:51 -0400

    Update GameOfLifePlane.java

--- GameOfLifePlane.java
+++ GameOfLifePlane.java
@@ -52,12 +52,13 @@ public class GameOfLifePlane extends Plane {
                             public void mousePressed(MouseEvent e) {
                                 isPressed = true;
                                 int position[] = cell.getPos();
-                                if( ((GameOfLifeElement)(cell.getElement())).checkAlive() == false){
+                                if( ((GameOfLifeElement)(cell.getElement())).checkAlive() == false ||
+                                        ((GameOfLifeElement)(cell.getElement())) == null){
                                     GameOfLifeElement el = new GameOfLifeElement(position[0], position[1], planeItSelf, cell, true);
                                     cell.addElement(el);
                                     
                                 	                
-		            }else{
+		            }else if(((GameOfLifeElement)(cell.getElement())).checkAlive() == true){
                                     cell.setBackground(Color.red);
                                     cell.setElement(null);
                                 }
@@ -87,22 +88,44 @@ public class GameOfLifePlane extends Plane {
     }
 	@Override
 	public void paintComponent(Graphics g) {
+            // make a new map that is same as the one we currently have
             Cell[][] newMap = map.clone();
+            // runthrough the length of the map array
             for(int i = 0; i < map[0].length; i++){
+                // run through the components
                 for(int j = 0; j < map.length; j++){
+                    // get the element at this current cell of the map
                     GameOfLifeElement el = (GameOfLifeElement)map[i][j].getElement();
+                    // if its null
                     if(el == null){
+                        // make el a new element that is dead
                         el = new GameOfLifeElement(i, j, this, map[i][j], false);
+                        // add it to the map
                         map[i][j].addElement(el);
                     }
-                    
-                    if(el.checkAlive()){
-                        newMap[i][j].addElement(el);
-                    }else{
+                    // if element is alive
+                    if(el.checkAlive() == true){
+                        // add it to the new map
+                        GameOfLifeElement newEl = new GameOfLifeElement(i, j, this, newMap[i][j], true);
                         
+                        
+                        newMap[i][j].addElement(newEl);
+                    }else{
+                        GameOfLifeElement newEl = new GameOfLifeElement(i, j, this, newMap[i][j], false);
+                        
+                        
+                        newMap[i][j].addElement(newEl);
+                        
+                       // if not dont add it
                     }
                 }
             }
+            for(int i = 0; i < map[0].length; i++){
+                for(int j = 0; j < map.length; j++){
+                    this.map[i][j].addElement(newMap[i][j].getElement());
+                }
+            }
+            // make the current map the new map
             this.map = newMap;
 		
 	
