571b5a9c58b15515122772fa75de93855b62b725
dawr3k <168124084+dawr3k@users.noreply.github.com>
2024-06-04 14:08:24 -0400

    Update GameOfLifeElement.java

--- GameOfLifeElement.java
+++ GameOfLifeElement.java
@@ -91,9 +91,10 @@ public class GameOfLifeElement extends Element{
     public int aliveNeighbours(){
         int numAliveNbs = 0;
         
-            for(int k = 0; k < neighbours.size()/2; k+=2){
+            for(int k = 0; k < neighbours.size(); k+=2){
 
-                if(plane.map[neighbours.get(k)][neighbours.get(k+1)].getElement() == null){
+                if(plane.map[neighbours.get(k)][neighbours.get(k+1)].getElement() == null ||
+                        ((GameOfLifeElement)(plane.map[neighbours.get(k)][neighbours.get(k+1)].getElement())).getAlive() == false){
            
         }else{
                   
@@ -106,14 +107,41 @@ public class GameOfLifeElement extends Element{
     }
     public void setAlive(boolean alive){
         this.alive = alive;
+        if(alive){
+        parent.setBackground(Color.black);
+        }else{
+            parent.setBackground(Color.white);
+        }
+        
+    }
+    public boolean getAlive(){
+        return alive;
     }
     public boolean checkAlive(){
+        // gives integer of neighbours that are alive
         int proof = this.aliveNeighbours();
-        if(proof >= 2 && proof <=3){
-            return true;
-        }else{
+        // if cell is alive and has less than two alive neighbours it dies
+        if(this.getAlive() == true && proof < 2){
+            
             return false;
+            // if its alive and has more than 3 alive neighbours
+            // its dead
+        }else if(this.getAlive() == true && proof > 3){
+            
+            return false;
+            // if its dead has 3 alive neighbours it is alive
+        }else if(this.getAlive() == false && proof == 3){
+            
+            return true;
+            // if its null and has 3 alive neighbours it becomes alive
+        }else if(this == null && proof == 3){
+            
+            return true;
         }
+            return alive;
+        
+        
+        
     }
     
     
