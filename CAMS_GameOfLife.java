016c979807290db69b1b36d632add893cbc8b214
januraama2006 <januraama2006@gmail.com>
2024-06-06 13:00:55 -0400

    Update CAMS_GameOfLife.java

--- CAMS_GameOfLife.java
+++ CAMS_GameOfLife.java
@@ -5,8 +5,20 @@ Game of Life frame
 package cams;
 
 import java.awt.BorderLayout;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JToolBar;
 
 /**
  *
@@ -20,10 +32,12 @@ public class CAMS_GameOfLife extends javax.swing.JFrame {
 
     public Cell[][] map;
 
-    public CAMS_GameOfLife(CAMS_Mainframe m) {
+    public CAMS_GameOfLife(CAMS_Mainframe m) throws IOException {
         initComponents();
-        // add it
-
+        main = m;
+        Cursor cursor;
+        cursor = Toolkit.getDefaultToolkit().createCustomCursor(new ImageIcon(getClass().getResource("/cams/pointer_scifi_b.png")).getImage(), new Point(), "Custom cursor");
+        setCursor(cursor);
     }
 
     /**
@@ -33,33 +47,79 @@ public class CAMS_GameOfLife extends javax.swing.JFrame {
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+    private void initComponents() throws IOException {
 
         map = new Cell[100][100];
 
-        //need to add toolbar to game of life as well, will implement later today
-	    
-        //the actual functionality of the plane does not work properly
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         plane = new GameOfLifePlane(map.length, map[0].length, map, this);
+        //need to add toolbar to game of life as well, will implement later today
+        //does not work properly
+        JToolBar toolbar = new JToolBar();
 
-        plane.setVisible(true);
+        
+
+        golHomeBtn = new javax.swing.JButton();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        golHomeBtn.setText("HOME");
+        
+
+        playAndPauseBtn = new javax.swing.JButton();
+
+        BufferedImage playAndPauseIcon = ImageIO.read(new File("src/cams/PlayAndPause.png"));
+        Image playAndPauseImage = playAndPauseIcon.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+
+        playAndPauseBtn.setIcon(new ImageIcon(playAndPauseImage));
+        playAndPauseBtn.setText("PLAY/PAUSE");
+        playAndPauseBtn.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        playAndPauseBtn.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        playAndPauseBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ((Plane) plane).playOrPause(evt);
+            }
+        });
+        
+
         add(plane, BorderLayout.CENTER);
+        
+        golHomeBtn.addActionListener((java.awt.event.ActionEvent evt) -> {
+            golHomeBtnActionPerformed(evt);
+        });
+        
+        
+
+        toolbar.add(golHomeBtn);
+        toolbar.addSeparator(new Dimension(50, 50)); //Creates a seperation line between the button and the toolbar
+
+        toolbar.add(playAndPauseBtn);
+        toolbar.setRollover(true);
+        toolbar.setFocusable(rootPaneCheckingEnabled);
+
+        add(toolbar, BorderLayout.PAGE_START);
+
         pack();
 
         setSize(1000, 800);
-        setTitle("Application");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("GAME OF LIFE");
+
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 // send back to first window
 
-    private void golHomeBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_golHomeBtnActionPerformed
-        this.setVisible(false);
+    private void golHomeBtnActionPerformed(java.awt.event.ActionEvent evt) {
         main.setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_golHomeBtnActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton golHomeBtn;
+    private javax.swing.JButton playAndPauseBtn;
+
     // End of variables declaration//GEN-END:variables
 }
+
 
