56ee64ad3f61f503b4817f78f98ba58aced3f489
zencue <84475875+zencue@users.noreply.github.com>
2024-06-09 17:38:20 -0400

    Update SourceWire.java

--- SourceWire.java
+++ SourceWire.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 import java.util.ArrayList;
 
 public class SourceWire extends Wire{
-	private Wire wire;
+	
 	
 	private int finish;//variable which detectes when to make electrons
 	private int counter;//variables which changes each time to rach a finish variable
@@ -28,35 +28,59 @@ public class SourceWire extends Wire{
 		finish = 5;
 		//adding this sourcewire to array list of all sourcewires
 		WireworldPlane.addSource(this);
-		wire = this;
-		//creating signals
+		//creating electrons
 		createSignals();
 	}
+	/**
+	     * Method which creates electrons
+	     *
+	     * @param none
+	     */
 	public void createSignals() {
-		if(counter == finish) {
-			for(int i =0;i<4;i++) {
+		if(counter == finish) {//if cunter reached finish
+			for(int i =0;i<4;i++) {//creating electrons
 				Electron e = new Electron(this.getX(),this.getY(),new int[] {speeds.get(i)[0],speeds.get(i)[1]});
 				electrons.add(e);
-				WireworldPlane.addElectron(e);
+				WireworldPlane.addElectron(e);//adding to arraylist of electrons
 				
 			}
 			
 			counter = 0;
 		}
-		else {
+		else {//otherwise adding one to counter
 			counter+=1;
 		}
 	}
+	/**
+	     * Method which checks if wire contins electrons
+	     *
+	     * @return boolean status
+	     */
 	public boolean doesContainElectrons() {
 		return electrons.size() != 0;
 	}
+	/**
+	     * Method which adds electrons to wire's arraylist
+	     *
+	     * @param Electron electron
+	     */
 	public void addElectron(Electron electron) {
 		electrons.add(electron);
 		
 	}
+	/**
+	     * Method which deletes all electrons from the arraylist
+	     *
+	     * @param none
+	     */
 	public void removeElectrons() {
 		electrons.clear();
 	}
+	/**
+	     * Method which returns arraylist of the electrons
+	     *
+	     * @param none
+	     */
 	public ArrayList<Electron> getElectrons() {
 		return electrons;
 	}
