d82985c376399a235299a7e79a75a94ede7d7a96
zencue <84475875+zencue@users.noreply.github.com>
2024-06-11 11:09:52 -0400

    Update ANDWire.java

--- ANDWire.java
+++ ANDWire.java
@@ -1,16 +1,25 @@
 import java.awt.Color;
-
+/* Janura,Fedor and Dawson
+ * 6/11/2024
+ * This is a class of AND logci wire. It extends the logic wires and inherits all neccassary logic wires methods. 
+*/
 public class ANDWire extends LogicWire{
-
+	/* Constructor of the ANDWIre class
+	 * @param: int x - x position of the wire
+  	 * @param: int y - y position of the wire
+     	* @param: int[] direction - direction of the logic wire
+	*/
 	public ANDWire(int x, int y, Cell parent,  int[] direction) {
 		super(x, y, parent, Color.cyan, direction);
-		// TODO Auto-generated constructor stub
+		
 	}
-
+	/* Methods which checks "logical" condition for specific type of the logic wire
+	 * @return: boolean status- logical checking
+	*/
 	@Override
 	public boolean condition() {
-		// TODO Auto-generated method stub
-		return electrons.size() == 2;
+		
+		return electrons.size() == 2;//if both entrance are "1", which means there are electrons there
 	}
 
 }
