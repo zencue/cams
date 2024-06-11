6466c4e8ccb2273271c05bffc96e2fffbed18957
zencue <84475875+zencue@users.noreply.github.com>
2024-06-11 11:21:01 -0400

    Update XORWire.java

--- XORWire.java
+++ XORWire.java
@@ -1,5 +1,8 @@
 import java.awt.Color;
-
+/* Janura,Fedor and Dawson
+ * 6/11/2024
+ * This is a class of XOR Wire which allows user to put logic xor operand in the plane 
+*/
 public class XORWire extends LogicWire{
 
 	public XORWire(int x, int y, Cell parent, int[] direction) {
