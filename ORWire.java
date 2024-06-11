6d89cfcf6fc19531b8f2070c6305ccc286e2222b
zencue <84475875+zencue@users.noreply.github.com>
2024-06-11 11:16:37 -0400

    Update ORWire.java

--- ORWire.java
+++ ORWire.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
 import java.util.ArrayList;
-
+/* Janura,Fedor and Dawson
+ * 6/11/2024
+ * This is a class of OR Wire which allows user to put logic or operand in the plane 
+*/
 public class ORWire extends LogicWire{
 	
 	public ORWire(int x, int y, Cell parent,int[] direction) {
