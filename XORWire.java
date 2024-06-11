87e8fcdbd43ea8b18f26e2711f0c718ca78e9b7a
zencue <84475875+zencue@users.noreply.github.com>
2024-06-11 11:22:47 -0400

    Update XORWire.java

--- XORWire.java
+++ XORWire.java
@@ -13,10 +13,12 @@ public class XORWire extends LogicWire{
 		super(x, y, parent, Color.MAGENTA, direction);
 		// TODO Auto-generated constructor stub
 	}
-
+	/* Methods which checks "logical" condition for specific type of the logic wire
+	 * @return: boolean status- logical checking
+	*/
 	@Override
 	public boolean condition() {
-		if(electrons.size() == 1) {
+		if(electrons.size() == 1) {//if there is only 1 electron in the wire, which is basically reprasination of the xor operand.
 			return true;
 		}
 		return false;
