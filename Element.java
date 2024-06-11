c1099ff195b3bdf02eca718b2d54f145206c41c9
januraama2006 <januraama2006@gmail.com>
2024-06-11 19:55:04 -0400

    Update Element.java

--- Element.java
+++ Element.java
@@ -1,3 +1,11 @@
+/*
+ * Janura, Fedor, and Dawson
+ * June 11th 2024
+ * This class is used as the superclass of all both GameOfLife elements and all wires in WireWorld (through Wire being a subclass of it)
+ * It is used as a placeholder class, similar to an abstract class, rather there is no "blueprint" methods or variables
+ */
+
+package cams;
 
 public class Element {
 
